b9cec54aedb41f5b4c45ea5b33e06360ac42d5ae
Camden Spehl <47334268+CamdenSpehl@users.noreply.github.com>
2025-12-07 14:17:18 -0800

    #22 sentry setup

--- classla-frontend/src/main.tsx
+++ classla-frontend/src/main.tsx
@@ -4,6 +4,28 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://341dfa3201eb011ef48792361056bf98@o4510491058896896.ingest.us.sentry.io/4510491061583872",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: false,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.,
+  // Enable logs to be sent to Sentry
+  enableLogs: true
+});
+
 // Create a client for React Query
 const queryClient = new QueryClient({
   defaultOptions: {
